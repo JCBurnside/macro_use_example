7f102fd0d967f91b1a78a4d40b1c21ec3f6260f4
James Burnside <jcburnside97@gmail.com>
2021-05-02 09:55:10 -0400

    UPDATED WITH MINOR FIXES

--- macro_sample/src/main.rs
+++ macro_sample/src/main.rs
@@ -1,10 +1,29 @@
 use macro_export::format;
+use macro_support::CustomTrait;
 
 #[format]
-fn test(_:&str) -> bool {
+fn test(foo:&str) -> bool {
     true
 }
+// struct test_t;
+
+//         impl macro_support::CustomTrait for test_t
+//         {
+//             const NAME : &'static str = "test";
+//             fn is_valid(&self,foo:&str) -> bool {
+//                 true
+//             }
+//         }
+
+//         const test : test_t = test_t {};
+
+//         impl ToString for test_t {
+//             fn to_string(&self) -> String {
+//                 format!("format: {}", <Self as macro_support::CustomTrait>::NAME).to_string()
+//             }
+//         }
 
 fn main() {
-    println!("Hello, world!");
+
+    println!("Hello, world!{}", "foo");
 }
